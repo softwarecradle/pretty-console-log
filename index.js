e7f909c0808188ee5d7d12fe44a3c589e6c543ea
softwarecradle <softwarecradleapps@gmail.com>
2022-02-16 02:29:26 +0530

    update code

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+const DEFAULTS = Object.freeze({
+    BACKGROUND: '#0000',
+    COLOR: '#777',
+    FONT: {
+        SIZE: 14
+    }
+})
+
+
 // print in color
 exports.pretty = {
     log(text, color) {
-        console.log(`%c${text}`, `color: ${color}`)
+        console.log(`%c${text ? text : ''}`, 
+            `background: ${DEFAULTS.BACKGROUND}; color: ${color ? color : DEFAULTS.COLOR}`)
     },
 
     log(text, color, fontSize) {
-        console.log(`%c${text}`, `color: ${color}; font-size: ${fontSize}px`)
+        console.log(`%c${text ? text : ''}`, 
+            `background: ${DEFAULTS.BACKGROUND}; color: ${color ? color : DEFAULTS.COLOR}; font-size: ${fontSize ? fontSize : DEFAULTS.FONT.SIZE}px`)
     }
 }
