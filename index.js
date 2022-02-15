b190f11ac282d96261bd613f6e9cea3248e995ec
softwarecradle <softwarecradleapps@gmail.com>
2022-02-16 01:01:47 +0530

    initial code

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// nothing for now
+// print in color
+module.exports = Object.freeze({
+    log(text, color) {
+        console.log(`%c${text}`, `color: ${color}`)
+    },
+
+    log(text, color, fontSize) {
+        console.log(`%c${text}`, `color: ${color}; font-size: ${fontSize}px`)
+    }
+})
